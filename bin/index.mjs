5378b583b87b832615176bc3b5f3b857389db89d
Carrington <carrdenn3@gmail.com>
2023-02-14 03:41:45 -0500

    Updating functionality for GameManager & GameState

--- bin/index.mjs
+++ bin/index.mjs
@@ -1,37 +1,5 @@
 #!/usr/bin/env node
 
-import chalk from "chalk";
-import boxen from "boxen";
-import yargs from "yargs/yargs";
-import readline from "readline";
-
-const argv = yargs(process.argv.slice(2)).argv;
-
-// let reader = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
-
-// reader.question(`What's your name?`, (name) => {
-//   console.log(`Hi ${name}!`);
-//   reader.close();
-// });
-
-// if (argv.ships > 3 && argv.distance < 53.5) {
-//   console.log("Plunder more riffiwobbles!");
-// } else {
-//   console.log("Retreat from the xupptumblers!");
-// }
-
-// const greeting = chalk.white.bold("Hello!");
-
-// const boxenOptions = {
-//   padding: 2,
-//   margin: 2,
-//   borderStyle: "round",
-//   borderColor: "green",
-//   backgroundColor: "#fff",
-// };
-// const msgBox = boxen(greeting, boxenOptions);
-
-// console.log(msgBox);
+import GameManager from "../src/gamemanager.mjs";
+const gameManager = new GameManager();
+gameManager.run();
--- src/gamemanager.mjs
+++ src/gamemanager.mjs
@@ -1,28 +1,69 @@
 import readline from "readline";
 import Gameboard from "./gameboard.mjs";
+import GameState from "./gamestate.mjs";
+import { SYMBOL } from "./constants.mjs";
 
 let reader = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
+const prompts = [
+  `${SYMBOL.X}'s turn ... select a square (1-9)\n`,
+  `${SYMBOL.Y}'s turn ... select a square (1-9)\n`,
+];
+
 export default class GameManager {
   state;
   board;
   constructor() {
+    this.state = new GameState();
     this.board = new Gameboard();
+    this.board.initiaze();
   }
 
-  run = () => {};
-
-  onUserInputSymbol = () => {
-    reader.question("Enter a key for your player symbol", (symbol) => {
-      console.log(`You entered ${symbol}!`);
-    });
-    reader.close();
+  transposePosition = (num) => {
+    switch (num) {
+      case 1:
+        return [0, 0];
+      case 2:
+        return [0, 1];
+      case 3:
+        return [0, 2];
+      case 4:
+        return [1, 0];
+      case 5:
+        return [1, 1];
+      case 6:
+        return [1, 2];
+      case 7:
+        return [2, 0];
+      case 8:
+        return [2, 1];
+      case 9:
+        return [2, 2];
+      default:
+        return [0, 0];
+    }
   };
 
-  onCloseUserInput = () => {
-    reader.close();
+  run = () => {
+    const prompt = prompts[this.state.turn];
+    reader.question(prompt, (input) => {
+      if (String(input).toLowerCase() === "exit") return reader.close();
+      if (!input || input < "1" || input > "9") {
+        this.run();
+      } else {
+        input = Number(input);
+        let symbol = this.state.getPlayerSymbol();
+        let pos = this.transposePosition(input);
+        let result = this.board.onFillSquare(pos[0], pos[1], symbol);
+        if (result) {
+          this.board.onPrint();
+          this.state.onUpdateTurn();
+        }
+        this.run();
+      }
+    });
   };
 }
